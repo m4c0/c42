b541e8b19725d3dc434180748eb7efe67b83f6ee
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-07 20:49:23 +0000

    trying to use c42 to parse glsl

---
+++ poc-leco.cpp
@@ -0,0 +1,48 @@
+#pragma leco tool
+import c42;
+import jojo;
+import jute;
+import print;
+import sv;
+
+struct defs : c42::defines {
+  bool has(sv name) const override {
+    return name == "_WIN32";
+  }
+};
+
+int main() try {
+  defs d {};
+
+  jute::view fn = "tests/shader.glsl";
+  auto buf = jojo::read_cstr(fn);
+  auto ctx = c42::preprocess(&d, buf);
+
+  const auto log = [&](auto t, jute::view lvl) {
+    errln(fn, ":", t.line, ":", t.column, ": [", lvl, "] ", ctx.txt(t));
+  };
+
+  bool has_error = false;
+  for (auto t : ctx) {
+    if (t.type == c42::t_warning) {
+      log(t, "warning");
+    } else if (t.type == c42::t_error) {
+      log(t, "error");
+      has_error = true;
+    }
+  }
+  if (has_error) return 1;
+
+  for (auto t : ctx) {
+    switch (t.type) {
+      using namespace c42;
+      case t_pragma:
+        putln(ctx.txt(t));
+        break;
+      default: break;
+    }
+  }
+  return 0;
+} catch (...) {
+  return 1;
+}
